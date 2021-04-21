9cf5445b7ab957613f073c424e9494d9670366d2
jinyand <jiiin97@naver.com>
2021-04-21 20:31:57 +0900

    Room 라이브러리 관련 클래스 구현

--- app/src/main/java/com/example/sampletodolist/database/TodoDatabase.kt
+++ app/src/main/java/com/example/sampletodolist/database/TodoDatabase.kt
@@ -1,9 +1,11 @@
 package com.example.sampletodolist.database
 
 import android.content.Context
+import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
 
+@Database(entities = [Todo::class], version = 1)
 abstract class TodoDatabase : RoomDatabase() {
     abstract fun todoDao() : TodoDao
 
